3fc841bd98c5244d43c995325a7462f7443b2e8d
CanzzD <canoozzdem1r@gmail.com>
2023-08-10 10:44:52 +0300

    First Commit

---
+++ GameProjectDemo/Entities/Games.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GameProjectDemo.Entities
+{
+    public class Games
+    {
+        public string GameName { get; set; }
+        public string Producer { get; set; }
+        public int Price { get; set; }
+
+    }
+}
